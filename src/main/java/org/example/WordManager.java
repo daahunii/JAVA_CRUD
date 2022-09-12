6fd907c3724afccb274ede04dd8a6a103848f1f1
daahunii <wjdekgns35@naver.com>
2022-09-12 18:57:57 +0900

    complete CRUD(add, searchLevel, searchWord)

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -33,11 +33,20 @@ public class WordManager {
             else if(menu == 1){
                 wordCRUD.listAll();
             }
+            else if(menu == 2){
+                wordCRUD.searchLevel();
+            }
+            else if(menu == 3){
+                wordCRUD.searchWord();
+            }
             else if(menu == 5){
                 wordCRUD.updateItem();
             }
             else if(menu == 6){
                 wordCRUD.deleteItem();
+            }
+            else if(menu == 7){
+                wordCRUD.saveFile();
             }
         }
     }
